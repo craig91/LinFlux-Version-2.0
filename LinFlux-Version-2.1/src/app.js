ba2a0f090ad80774dbe4a58ad5dcefbbae6ab54c
craig91 <craigdejean1@gmail.com>
2017-03-05 03:25:37 -0500

    configured react router

--- LinFlux-Version-2.1/src/app.js
+++ LinFlux-Version-2.1/src/app.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router';
-import { Splash, Root, Nav } from './components';
 
+import Nav from './components/nav.jsx';
+import Splash from './components/splash.jsx';
+import Root from './components/root.jsx';
+import HomePage from './components/pages/homePage.jsx';
+
+const App = React.createClass({
+  render: function(){
+    return(
+      <div>
+        {this.props.children}
+      </div>
+    )
+  }
+})
 
 
 
@@ -10,11 +23,12 @@ import { Splash, Root, Nav } from './components';
 
 ReactDOM.render((
   <Router history={hashHistory}>
-   <Route path="/" component={Root}>
-     <IndexRoute component={Splash} />
+   <Route path="/" component={App}>
+     <IndexRoute component={Root} />
        <Route path="nav" component={Nav} />
+       <Route path="root" component={Root} />
+       <Route path="homePage" component={HomePage} />
+       <Route path="splash" component={Splash} />
    </Route>
   </Router>
 ),document.getElementById('root'));
-
-
