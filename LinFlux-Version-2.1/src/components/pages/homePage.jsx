fd1edc04eafd98bf5dbfe3c8a78d75a98867f195
craig91 <craigdejean1@gmail.com>
2017-03-05 03:31:54 -0500

    added pages directory

---
+++ LinFlux-Version-2.1/src/components/pages/homePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../../public/css/homePage.css';
+
+
+const HomePage = React.createClass({
+  render: function() {
+    return(
+      <div className="bg-color">
+
+        <img src={"http://8-themes.com/wp-content/uploads/2015/12/Space_View_of_Saturn_with_its_moon_094903_-2208x1242.jpg"} alt="bg" className="bg" />
+      </div>
+    )
+  }
+})
+
+export default HomePage;
