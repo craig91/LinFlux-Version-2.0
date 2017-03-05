46cfa97c713e0f76a737177b23a31b638402c765
craig91 <craigdejean1@gmail.com>
2017-03-05 03:24:56 -0500

    changed how components are exported

--- LinFlux-Version-2.1/src/components/index.jsx
+++ LinFlux-Version-2.1/src/components/index.jsx
@@ -1,5 +1,6 @@
-import Root from './root.jsx';
-import Splash from './splash.jsx';
-import Nav from './nav.jsx';
-
-export {Root, Splash, Nav}
+// import Root from './root.jsx';
+// import Splash from './splash.jsx';
+// import Nav from './nav.jsx';
+// import HomePage from './pages/homePage.jsx';
+//
+// export {Root, Splash, Nav, HomePage};
