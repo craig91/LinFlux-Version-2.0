fdb1add389a7ddb6d75dd0ed9a7636a9a2b31fa0
craig91 <craigdejean1@gmail.com>
2017-03-11 00:02:21 -0500

    added home page

--- LinFlux-Version-2.1/src/components/pages/homePage.jsx
+++ LinFlux-Version-2.1/src/components/pages/homePage.jsx
@@ -6,9 +6,12 @@ const HomePage = React.createClass({
   render: function() {
     return(
       <div className="bg-color">
-
-        <img src={"http://8-themes.com/wp-content/uploads/2015/12/Space_View_of_Saturn_with_its_moon_094903_-2208x1242.jpg"} alt="bg" className="bg" />
+         <img src={"http://8-themes.com/wp-content/uploads/2015/12/Space_View_of_Saturn_with_its_moon_094903_-2208x1242.jpg"} alt="bg" className="bg" />
+         <div className="h-class-slogan">
+           <div className="signUp"><p id="signUp-text">Sign Up</p></div>
+         </div>
       </div>
+
     )
   }
 })
