2e0c0053ec52758f024098d37bfa8d8e3b5f7a69
craig91 <craigdejean1@gmail.com>
2017-03-15 12:15:10 -0400

    added test seed file for blog posts

---
+++ linflux_backend/seed/blog_seed.js
@@ -0,0 +1,16 @@
+const Sequelize  = require('../models').Sequelize;
+const sequelize = require('../models').sequelize;
+
+
+const Blog = require('../models').Blog;
+
+Blog.sync({force: true})
+.then(()=> Blog.bulkCreate([
+  {
+    hero: 'https://www.kali.org/wp-content/uploads/2014/06/kali-wp-june-2014_1920x1080_B.png',
+    title: 'Kali Linux',
+    sDescript: 'Penetration Testing distro. For security professionals'
+  },
+]))
+
+.catch((err) => console.log(err));
