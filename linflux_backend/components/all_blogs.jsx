f16fc6de3249b6e3b7e3b94337c47441cb9be179
craig91 <craigdejean1@gmail.com>
2017-03-15 23:35:11 -0400

    added blogs component

---
+++ linflux_backend/components/all_blogs.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import $ from 'jquery';
+import {Link} from 'react-router';
+
+
+const Allblogs = React.createClass({
+  getInitialState() {
+    return {
+      blogs: []
+    }
+  },
+  componentDidMount() {
+    {
+      $.ajax({
+        url: '/api/blogs',
+        type: 'GET'
+      })
+      .done((data) => {
+        console.log(data)
+        this.setState({ blogs: data })
+      })
+    }
+  },
+  render: function() {
+    return (
+      <div>
+        <div>{this.state.blogs.length === 0 ? "Loading" : this.state.blogs.map(function(val, idx){
+          return (
+            <div key={idx}>
+            <div><img src={val.hero} /></div>
+
+            <h1>{val.title}</h1>
+            <h2>{val.sDescript}</h2>
+          </div>
+          )
+        })}  </div>
+      </div>
+    )
+  }
+})
+
+
+
+export default Allblogs;
