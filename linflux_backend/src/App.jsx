bfd7d64dc049a65d64a8cef7f14099314ee9b9a6
craig91 <craigdejean1@gmail.com>
2017-03-15 23:34:21 -0400

    added all blogs route to react router

--- linflux_backend/src/App.jsx
+++ linflux_backend/src/App.jsx
@@ -6,6 +6,7 @@ import { IndexRoute, Link, Router, Route, browserHistory, hashHistory } from 're
 import Greeting from '../components/hello.jsx';
 import CreateUser from '../components/userForm.jsx';
 import EditUser from '../components/editUser.jsx';
+import Allblogs from '../components/all_blogs.jsx';
 
 
 var App = React.createClass({
@@ -24,6 +25,7 @@ ReactDOM.render (
       <IndexRoute component={Greeting} />
       <Route path='/userForm' component={CreateUser} />
       <Route path='/editUser/:id' component={EditUser} />
+      <Route path='/allBlogs' component={Allblogs} />
     </Route>
   </Router>,
   document.getElementById('root')
