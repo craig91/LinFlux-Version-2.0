21a8a5ab388bb8b8675ada8b8b4e85be97772944
craig91 <craigdejean1@gmail.com>
2017-03-17 18:07:10 -0400

    renamed blog model to article model

---
+++ linflux_backend/models/article_model.js
@@ -0,0 +1,31 @@
+'use strict'
+
+module.exports = function(sequelize, DataTypes) {
+  var Article = sequelize.define('Article', {
+    hero: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true,
+      validate: {
+        isUrl: true
+      }
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true
+    },
+    sDescript: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true
+    }
+  },{
+    classMethods: {
+      associate: function(models) {
+        Article.belongsTo(models.User);
+      }
+    }
+  });
+  return Article;
+};
