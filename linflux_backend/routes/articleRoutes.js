5bd649fd95ac7db11921f083b9b883f86ebb5728
craig91 <craigdejean1@gmail.com>
2017-03-17 19:47:24 -0400

    fixed routing issue

--- linflux_backend/routes/articleRoutes.js
+++ linflux_backend/routes/articleRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const Article = require('../models').Article;
+const User = require('../models').User;
 const models = require('../models/index');
+
 
 function allArticles(req, res) {
   Article.findAll({}).then(function(data) {
@@ -11,7 +13,20 @@ function allArticles(req, res) {
   })
 }
 
+function OneArticle(req, res) {
+  Article.findById(req.params.id, {
+    include : [User]
+  })
+  .then(function(OneArticle){
+    res.send(OneArticle)
+  })
+  .catch((err) => console.log('one Article', error))
+}
+
 router.route('/')
 .get(allArticles)
+
+router.route('/:id')
+.get(OneArticle)
 
 module.exports = router;
