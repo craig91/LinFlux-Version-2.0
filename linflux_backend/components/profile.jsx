e84dab4e052ced21b84fa0a8ff546c6056cd4f64
craig91 <craigdejean1@gmail.com>
2017-03-17 18:06:24 -0400

    added profile component for user

---
+++ linflux_backend/components/profile.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import $ from 'jquery';
+import {Link} from 'react-router';
+
+
+const OneUser = React.createClass({
+  getInitialState() {
+    return {
+      user: null
+    }
+  },
+  componentDidMount() {
+    var apiCall = '/api/users/' + this.props.params.id
+    console.log(apiCall)
+   {
+    $.ajax({
+      url: apiCall,
+      type: 'GET'
+    })
+    .done((data) => {
+      console.log(data)
+      this.setState({ user: data })
+    })
+   }
+ },
+  render: function(){
+    console.log('users', this.state.user)
+    return (
+
+      <div>{this.state.user === null ? 'Loading......' : this.state.user.Articles.map(function(val, idx) {
+          return (
+            <div key={idx}>
+               <Link to={"/articles/" + val.id} key={idx}>{val.title}</Link>
+            </div>
+          )
+      })}</div>
+    )
+  }
+})
+
+
+export default OneUser;
