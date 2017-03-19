900dca9bc734623aff0fa2958c797f05a3f9f6ab
craig91 <craigdejean1@gmail.com>
2017-03-19 17:29:39 -0400

    added article form to post new articles

---
+++ linflux_backend/components/articleForm.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import $ from 'jquery';
+import { Link } from 'react-router';
+
+const ArticleForm = React.createClass({
+  getInitialState() {
+    return { article: null }
+  },
+  handleSubmit(event) {
+    {
+      $.ajax({
+        url: 'api/articles',
+        type: 'POST',
+        data: {
+          hero: this.hero.value,
+          title: this.title.value,
+          sDescript: this.sDescript.value
+        }
+      }).done((data) => {
+        console.log(data)
+        this.setState({ article: data})
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+    console.log(this.hero.value)
+    console.log(this.title.value)
+    console.log(this.sDescript.value)
+  },
+  render: function() {
+    console.log(this.state.article)
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <span> Article Image </span>
+          <input type="text" ref={ (input) => {
+            this.hero = input;
+          }} required/>
+
+
+          <span> Title of your article </span>
+          <input type="text" ref={ (input) => {
+            this.title = input;
+          }} required/>
+
+
+          <span> A short description of your article </span>
+          <input type="text" ref={ (input) => {
+            this.sDescript = input;
+          }} required/>
+
+          <input type="submit" />
+        </form>
+      </div>
+    )
+  }
+})
+
+export default ArticleForm;
