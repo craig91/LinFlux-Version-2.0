8b10221191980b8ed9e2fa1bf04f19aae36ab1cd
craig91 <craigdejean1@gmail.com>
2017-03-19 17:27:32 -0400

    added default distros route

--- linflux_backend/routes/index.js
+++ linflux_backend/routes/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use('/users', require('./userRoutes.js'));
 router.use('/articles', require('./articleRoutes.js'));
+router.use('/distros', require('./distroRoutes.js'));
 
 module.exports = router;
