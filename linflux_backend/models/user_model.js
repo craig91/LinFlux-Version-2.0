e8bb2354dd73f9493481fd41b888e0a9a757ba07
craig91 <craigdejean1@gmail.com>
2017-03-22 16:30:49 -0400

    added more information for user

--- linflux_backend/models/user_model.js
+++ linflux_backend/models/user_model.js
@@ -8,10 +8,68 @@ module.exports = function(sequelize, DataTypes) {
       isAlpha: true
     },
     lastName: {
-      type: DataTypes.STRING,
+     type: DataTypes.STRING,
       allowNull: false,
       isAlpha: true
-    }
+    },
+    location: {
+     type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true
+    },
+    company: {
+     type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true
+    },
+    contact: {
+     type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true,
+      validate: {
+        isUrl: true
+      }
+    },
+    website: {
+     type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true,
+      validate: {
+        isUrl: true
+      }
+    },
+    linkedIn: {
+     type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true,
+      validate: {
+        isUrl: true
+      }
+    },
+    facebook: {
+     type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true,
+      validate: {
+        isUrl: true
+      }
+    },
+    twitter: {
+     type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true,
+      validate: {
+        isUrl: true
+      }
+    },
+    github: {
+     type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true,
+      validate: {
+        isUrl: true
+      }
+    },
   },{
     classMethods: {
       associate: function(models) {
