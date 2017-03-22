5580899af110fb128ac4eb553039bbef9200aeec
craig91 <craigdejean1@gmail.com>
2017-03-22 16:32:15 -0400

    added more default data for user

--- linflux_backend/seed/user_seed.js
+++ linflux_backend/seed/user_seed.js
@@ -1,38 +1,85 @@
 const Sequelize = require('../models').Sequelize;
-// const sequelize = require('../models').sequelize;
 const User = require('../models').User;
 
 User.sync({force: true})
 .then(()=> User.bulkCreate([
-
 {
   firstName: 'Craig',
-  lastName: 'Dejean'
+  lastName: 'Dejean',
+  location: 'Brooklyn',
+  company: 'C4Q',
+  contact: 'craigdejean1@gmail.com',
+  website:'https://spotify-crud.herokuapp.com/?',
+  linkedIn: 'https://www.linkedin.com/in/craigdejean/',
+  facebook:'https://www.facebook.com/thelegendarycraig',
+  twitter:'https://twitter.com/t3chcraig1',
+  github: 'https://github.com/craig91'
 },
 
 {
   firstName: 'Ayuka',
-  lastName: 'Niga'
+  lastName: 'Niga',
+  location: 'Kyoto',
+  company: 'Japas 38',
+  contact: '',
+  website:'',
+  linkedIn: '',
+  facebook:'',
+  twitter:'',
+  github: ''
 },
 
 {
   firstName: 'Kamil',
-  lastName: 'Skorupa'
+  lastName: 'Skorupa',
+  location: 'Poland',
+  company: 'Brooklyn College',
+  contact: '',
+  website:'',
+  linkedIn: '',
+  facebook:'',
+  twitter:'',
+  github: ''
 },
 
 {
   firstName: 'Chris',
-  lastName: 'Samuel'
+  lastName: 'Samuel',
+  location: 'Brooklyn',
+  company: 'Jet Blue',
+  contact: '',
+  website:'',
+  linkedIn: '',
+  facebook:'',
+  twitter:'',
+  github: ''
+
 },
 
 {
   firstName: 'Nathaniel',
-  lastName: 'Brown'
+  lastName: 'Brown',
+  location: 'Bronx',
+  company: 'Construction',
+  contact: '',
+  website:'',
+  linkedIn: '',
+  facebook:'',
+  twitter:'',
+  github: ''
 },
 
 {
   firstName: 'Lance',
-  lastName: 'Jones'
+  lastName: 'Jones',
+  location: 'Bronx',
+  company: 'New York Department of Labor',
+  contact: '',
+  website:'',
+  linkedIn: '',
+  facebook:'',
+  twitter:'',
+  github: ''
 },
 
 ]))
