a9adbe6fad46abe1fb4687c789d48ad7bf266f38
craig91 <craigdejean1@gmail.com>
2017-03-22 16:31:28 -0400

    added more articles for seeding

--- linflux_backend/seed/article_seed.js
+++ linflux_backend/seed/article_seed.js
@@ -1,5 +1,4 @@
 const Sequelize  = require('../models').Sequelize;
-const sequelize = require('../models').sequelize;
 const Article = require('../models').Article;
 
 
@@ -9,7 +8,43 @@ Article.sync({force: true})
     hero: 'https://www.kali.org/wp-content/uploads/2014/06/kali-wp-june-2014_1920x1080_B.png',
     title: 'Kali Linux',
     sDescript: 'Penetration Testing distro. For security professionals',
-    UserId: 1
+    UserId: 1,
+    articleId: 1
+  },
+  {
+    hero: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Archlinux-vert-dark.svg/2000px-Archlinux-vert-dark.svg.png',
+    title: 'Arch Linux',
+    sDescript: 'Customization at its finest! Use Arch linux',
+    UserId: 1,
+    articleId: 1
+  },
+  {
+    hero: 'https://linuxpeer.org/assets/img/distro_logo/mint.png',
+    title: 'Linux Mint',
+    sDescript: 'Simple, Lightweight Linux Distro for beginners',
+    UserId: 2,
+    articleId: 1
+  },
+  {
+    hero: 'http://i.imgur.com/9KXZaVU.png',
+    title: 'XFCE Desktop Environment',
+    sDescript: 'Simple, Beginner friendly',
+    UserId: 3,
+    articleId: 1
+  },
+  {
+    hero: 'http://fossbytes.com/wp-content/uploads/2016/08/fedora-25-features.jpg',
+    title: 'Fedora Linux',
+    sDescript: 'Red Hat Linux variant',
+    UserId: 3,
+    articleId: 1
+  },
+  {
+    hero: 'http://www.rkdesigns.org/wp-content/uploads/bfi_thumb/slackao-18082008-30jg9z5uolqwx2xtg3en7k.jpg',
+    title: 'Slackware Linux',
+    sDescript: 'Old School Linux distro',
+    UserId: 4,
+    articleId: 1
   },
 ]))
 
