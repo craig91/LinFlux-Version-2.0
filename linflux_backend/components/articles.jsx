44199e1bef0fe28f0d152eae30955aeaea724563
craig91 <craigdejean1@gmail.com>
2017-03-28 18:26:15 -0400

    deleted placeholder text

--- linflux_backend/components/articles.jsx
+++ linflux_backend/components/articles.jsx
@@ -22,44 +22,50 @@ const Article = React.createClass({
              let article = this.state.articles;
              console.log(article)
              return(
-               <div className="hero-container" style={{backgroundImage: `url(${article.hero})` }}></div>
+               <div className="hero-container" style={{backgroundImage: `url(${article.hero})` }}>
+               
+    
+               
+               
+               </div>
+               
              )
            } else {
              return (
                <div>Loading....</div>
              )
            }
-
-        // return (
-        //     <div>
-        //         <div>{this.state.articles === null
-        //                 ? "Loading...."
-        //                 : this.state.articles.map(function(val, idx) {
-        //                     return (
-        //                         <div key={idx}>
-        //
-        //
-        //
-        //                             <div className="hero-container" style={{backgroundImage: `url(${val.hero})` }}>
-        //
-        //                             </div>
-        //                             <div className="blog-text">
-        //                                 Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Pellentesque in ipsum id orci porta dapibus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Nulla quis lorem ut libero malesuada feugiat. Nulla quis lorem ut libero malesuada feugiat.
-        //
-        //
-        //                             </div>
-        //
-        //
-        //
-        //
-        //
-        //                         </div>
-        //                     )
-        //                 })}
-        //         </div>
-        //     </div>
-        // )
     }
 })
 
 export default Article;
+
+// return (
+//     <div>
+//         <div>{this.state.articles === null
+//                 ? "Loading...."
+//                 : this.state.articles.map(function(val, idx) {
+//                     return (
+//                         <div key={idx}>
+//
+//
+//
+//                             <div className="hero-container" style={{backgroundImage: `url(${val.hero})` }}>
+//
+//                             </div>
+//                             <div className="blog-text">
+//                                 Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Pellentesque in ipsum id orci porta dapibus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Nulla quis lorem ut libero malesuada feugiat. Nulla quis lorem ut libero malesuada feugiat.
+//
+//
+//                             </div>
+//
+//
+//
+//
+//
+//                         </div>
+//                     )
+//                 })}
+//         </div>
+//     </div>
+// )
