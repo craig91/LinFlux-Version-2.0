05e9adb4b5f0c09661c9a69987bf6b43d7bcf2b4
craig91 <craigdejean1@gmail.com>
2017-04-12 00:07:44 -0400

    added nav bar, background color

--- linflux_backend/components/homepage.jsx
+++ linflux_backend/components/homepage.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router';
-
+import {Link} from 'react-router';
+import NavBar from './nav_bar.jsx';
 
 const Home = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <ul>
-          <li>Sign Up</li>
-          <li>Log In</li>
-          <Link to={'/distros'}> <li>Distros</li> </Link>
-          <li>About</li>
-        </ul>
+    render: function() {
+        return (
+            <div className="home-body">
+                <div><NavBar/></div>
 
-     </div>
-    )
-  }
+                <div className="image_container">
+
+                    <img className="home_page_background" src="../public/images/Splash_back2.png"></img>
+
+                </div>
+                {/* <div className="home_page_slogan">
+                   Linux Lives Here!
+                   All the linux info in one place!
+                </div> */}
+            </div>
+        )
+    }
 })
-
 
 export default Home;
