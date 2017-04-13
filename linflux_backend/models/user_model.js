e10df8f46b1442b4beef293fd5730a8930d8e0a7
craig91 <craigdejean1@gmail.com>
2017-04-13 00:38:39 -0400

    added password entry/validation & added hooks

--- linflux_backend/models/user_model.js
+++ linflux_backend/models/user_model.js
@@ -1,4 +1,6 @@
 'use strict';
+const Promise = require('bluebird');
+const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'))
 
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
@@ -8,6 +10,11 @@ module.exports = function(sequelize, DataTypes) {
       isAlpha: true,
       len: [7, 14]
     },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true
+    },
     firstName: {
      type: DataTypes.STRING,
       allowNull: false,
@@ -16,24 +23,46 @@ module.exports = function(sequelize, DataTypes) {
     },
     lastName: {
      type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
       isAlpha: true,
       len:[2, 50]
     },
     location: {
      type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
       isAlpha: true
     },
     company: {
      type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
        isAlpha: true
     },
   },{
     classMethods: {
       associate: function(models) {
         User.hasMany(models.Article);
+      }
+    },
+    instanceMethods: {
+      validPassword: function(password) {
+      return bcrypt.compareSync(password, this.password);
+      }
+    },
+    hooks: {
+      beforeCreate: function(user, options) {
+        const SALT_FACTOR = 10;
+            if (!user.changed('password')) {
+              return sequelize.Promise.reject("not modified");
+            }
+        return bcrypt.genSaltAsync(SALT_FACTOR).then(function(salt) {
+          return bcrypt.hashAsync(user.password, salt, null);
+        })
+        .then(function(hash) {
+          user.setDataValue('password', hash);
+        })
+        .catch(function(err) {
+          return sequelize.Promise.reject(err);
+        });
       }
     }
 });
