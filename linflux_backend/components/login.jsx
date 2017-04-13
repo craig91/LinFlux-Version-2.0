a88c400024f197f5dacf56122a998593baffba80
craig91 <craigdejean1@gmail.com>
2017-04-13 00:45:17 -0400

    created login component

---
+++ linflux_backend/components/login.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+
+const LoginForm = React.createClass({
+  getInitialState() {
+    return { user: null }
+  },
+  handleSubmit(event) {
+    event.preventDefault();
+    {
+      $.ajax({
+        url: 'api/users/login',
+        type: 'POST',
+        data: {
+          userName: this.userName.value,
+          password: this.password.value
+       }
+      }).done((data) => {
+        this.setState({ user: data })
+        console.log(data)
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+    console.log(this.userName.value)
+  },
+  render: function() {
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit} >
+
+          <span>Enter Your User Name</span>
+          <input type="text" placeholder="7 to 14 characters" ref={(input) => {
+            this.userName = input;
+          }} required/>
+
+          <span>Password</span>
+          <input type="text" ref={(input) =>{
+            this.password = input;
+          }} required/>
+
+
+          <input type="submit" />
+        </form>
+      </div>
+    )
+  }
+})
+
+
+
+export default LoginForm;
