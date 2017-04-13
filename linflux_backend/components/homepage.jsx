ac9a7a31ba77a177858e210df965a17cfab0fece
craig91 <craigdejean1@gmail.com>
2017-04-13 00:33:23 -0400

    created splash page slogan

--- linflux_backend/components/homepage.jsx
+++ linflux_backend/components/homepage.jsx
@@ -7,16 +7,11 @@ const Home = React.createClass({
         return (
             <div className="home-body">
                 <div><NavBar/></div>
-
-                <div className="image_container">
-
-                    <img className="home_page_background" src="../public/images/Splash_back2.png"></img>
-
+                  <img className="home_page_background" src="../public/images/Splash_back2.png"></img>
+                <div className="home_page_slogan">
+                   <p>You & Linux</p>
+                   <p>A Love story</p>
                 </div>
-                {/* <div className="home_page_slogan">
-                   Linux Lives Here!
-                   All the linux info in one place!
-                </div> */}
             </div>
         )
     }
