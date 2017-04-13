55fa53c7d71211c337609b8944755a8ca1dfa6ae
craig91 <craigdejean1@gmail.com>
2017-04-13 00:31:41 -0400

    added react modal

--- linflux_backend/components/nav_bar.jsx
+++ linflux_backend/components/nav_bar.jsx
@@ -3,9 +3,39 @@ import Greeting from './hello.jsx';
 import ArticleForm from './articleForm.jsx';
 import Distros from './distros.jsx';
 import CreateUser from './userForm.jsx';
+import Modal from 'react-modal';
 import {Link} from 'react-router';
 
+const modalStyles = {
+  content : {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroudColor: '#f0f1f1',
+    height: '30%',
+    width: '30%'
+  }
+};
+
 const NavBar = React.createClass({
+  getInitialState() {
+    return {
+      user: null,
+      showModal: false
+    }
+  },
+  componentWillUpdate(nextProps) {
+    console.log(nextProps.location.query)
+    if(nextProps.location.query['login']) {
+      this.props.router.replace('./profile/' + nextProps.location.query['login'])
+      this.setState({
+        user: {id: nextProps.location.query['login']}
+      })
+    }
+  },
     render: function() {
         return (
             <div>
@@ -13,7 +43,7 @@ const NavBar = React.createClass({
                     <Link to="/hello" className="active">Hello</Link>
                     <Link to="/articleForm">Article Form</Link>
                     <Link to="/distros">Distros</Link>
-                    <Link to="/userForm">User Form</Link>
+                    <Link to="/userForm">Sign in/Sign up</Link>
                 </nav>
             </div>
         )
