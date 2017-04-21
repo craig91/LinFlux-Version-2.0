c6e46b7b5ecda7a97388ee8e4f4e6ece93399afe
craig91 <craigdejean1@gmail.com>
2017-04-20 21:36:09 -0400

    added in profile info component

--- linflux_backend/components/profile.jsx
+++ linflux_backend/components/profile.jsx
@@ -4,6 +4,7 @@ import {Link} from 'react-router';
 import ProfileInfo from './profile_info.jsx';
 
 
+
 const OneUser = React.createClass({
   getInitialState() {
     return {
@@ -11,8 +12,8 @@ const OneUser = React.createClass({
     }
   },
   componentDidMount() {
-    var apiCall = '/api/users/' + this.props.params.id
-    console.log(apiCall)
+    var apiCall = '/api/users/private'
+    // console.log(apiCall)
    {
     $.ajax({
       url: apiCall,
@@ -27,19 +28,35 @@ const OneUser = React.createClass({
   render: function(){
     console.log('users', this.state.user)
     return (
+      <div>
+          <div>{this.state.user === null ? 'Loading......' : this.state.user.Articles.map(function(val, idx) {
+              return (
+                <div key={idx}>
+                   <Link to={"/articles/" + val.id} key={idx}>{val.title}</Link>
+               </div>
+              )
+            })
+          }
+          </div>
+          <div>
+            <ProfileInfo />
+            {/* {this.state.user === null ? 'Loading......' :
+              <div>
 
-      <div>{this.state.user === null ? 'Loading......' : this.state.user.Articles.map(function(val, idx) {
-          return (
-            <div key={idx}>
-               <Link to={"/articles/" + val.id} key={idx}>{val.title}</Link>
-               <p>{val.firstName}</p>
-               {/* <ProfileInfo /> */}
-            </div>
-          )
-      })}</div>
-    )
-  }
-})
+                    <p>{this.state.user.userName}</p>
+                    <p>{this.state.user.firstName}</p>
+                    <p>{this.state.user.lastName}</p>
+                    <p>{this.state.user.company}</p>
+                    <p>{this.state.user.location}</p>
+
+
+              </div>
+
+          } */}
+        </div>
+    </div>
+  )
+}})
 
 
 export default OneUser;
