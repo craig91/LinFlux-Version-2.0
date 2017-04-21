372d4f400a4cc7a0579e6d5f0acaf19586d0d76a
craig91 <craigdejean1@gmail.com>
2017-04-20 21:45:42 -0400

    recieves data from api

--- linflux_backend/components/profile_info.jsx
+++ linflux_backend/components/profile_info.jsx
@@ -4,38 +4,41 @@ import $ from 'jquery';
 
 const ProfileInfo = React.createClass({
   getInitialState() {
-    return {user: null}
+    return {
+      user: null
+    }
   },
   componentDidMount() {
+    const apiCall = '/api/users/private'
+    {
       $.ajax({
-        url: '/api/users/' + this.props.params.id,
-        type: 'GET',
-        data: {
-          userName: this.userName.value,
-          firstName: this.firstName.value
-        }
+        url: apiCall,
+        type: 'GET'
       })
       .done((data) => {
-        console.log(data)
-        this.setState({user: data})
+        this.setState({ user: data})
       })
-
-  } ,
-  render: function() {
-    return(
-        <div>
-          <div>{this.state.user.length === 0 ? "loading..." : this.state.user.map(function(val, idx) {
-            return (
-              <div key={idx}>
-                <p>{val.userName}</p>
-              </div>
-            )
-          }) }</div>
-        </div>
-    )
     }
+  },
+  render: function() {
+    console.log('users', this.state.user)
+    return(
+      <div>
+        {this.state.user === null ? 'Loading...' :
+          <div>
+            <p>{this.state.user.userName}</p>
+            <p>{this.state.user.firstName}</p>
+            <p>{this.state.user.lastName}</p>
+            <p>{this.state.user.company}</p>
+            <p>{this.state.user.location}</p>
 
-  })
+          </div>
+
+      }
+      </div>
+    )
+  }
+})
 
 export default ProfileInfo;
 
